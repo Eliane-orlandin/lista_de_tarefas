3acf6830a328a481544271604d2de7631b402f0d
Eliane <liorlandin33@gmail.com>
2023-09-25 21:49:44 -0300

    Adiciona funcao pegarInput

---
+++ script.js
@@ -0,0 +1,14 @@
+function pegarInput(){
+    // Acessa classe document e busca elemento pelo id
+    var inputElement = document.getElementById("input-produto")
+    // Imprime o valor do elemento encontrado
+    console.log(inputElement.value)
+    /* TODO - Implementa função que cria um elemento de linha
+    com o valor capturado
+    */ 
+
+    // Apaga o valor do elemento encontrado
+    inputElement.value = ''
+    // Move o foco para o elemento encontrado
+    inputElement.focus()
+}
